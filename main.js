b377249b238b4704aee9f3ec228aa333f0ebfec0
EC2 Default User <ec2-user@ip-172-31-45-46.ap-southeast-2.compute.internal>
2023-10-25 04:05:33 +0000

    電卓課題提出

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-//let plus = document.getElementById("plus");
-//let minus =document.getElementById("minus");
-//let asterisk = document.getElementById("asterisk");
-//let slash = document.getElementById("slash");
-//let equal = document.getElementById("equal");
-//let dot = document.getElementById("dot");
-//let zero2 = document.getElementById("zero2");
+let marks = ["+","-","*","/","."];
+let result = document.getElementById("result");
+let zero2 = document.getElementById("zero2");
+zero2.disabled = true;
 
-//plus.disabled = true;
-//asterisk.disabled = true;
-//slash.disabled = true;
-//equal.disabled = true;
-//zero2.disabled = true;
-
-function clickButton(target){
-  let result = document.getElementById("result");
-  let target_value = target.innerHTML;
   
+function clickButton(target){
+  let target_value = target.innerHTML;
+
   if (target_value == "AC") {
     result.innerHTML = "0";
     
   } else if (target_value == "=") {
     result.innerHTML = eval(result.innerHTML);
+    
+  } else if (marks.includes(target_value)) { 
+    if (marks.includes(result.innerHTML.slice(-1))) {
+      result.innerHTML = result.innerHTML.slice(0,-1) + target_value;
+    } else {
+      result.innerHTML += target_value;
+    }
     
   } else {
     if (result.innerHTML == "0") {
@@ -30,14 +28,10 @@ function clickButton(target){
       result.innerHTML += target_value;
     }
   }
-}
-
-function operator(target) {
-  let result = document.getElementById("result");
-  let target_value = target.innerHTML;
   
-  if (result.innerHTML.endsWith("+") == true || result.innerHTML.endsWith("-") == true || result.innerHTML.endsWith("*") == true || result.innerHTML.endsWith("/") == true || result.innerHTML.endsWith(".") == true) {
-    //result.innerHTML = result.innerHTML.slice(0,-1) + target_value;
-    console.log(result.innerHTML.slice(0,-1));
+  if (result.innerHTML == "0") {
+    zero2.disabled = true;
+  } else {
+    zero2.disabled = false;
   }
 }
